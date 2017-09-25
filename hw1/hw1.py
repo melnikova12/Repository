123d142c47806aa850b7dcbcc12560745c06e790
melnikova12 <arinabloom1@gmail.com>
2017-09-25 19:55:10 +0300

    Первая домашка

---
+++ hw1/hw1.py
@@ -0,0 +1,12 @@
+a = int(input("Введите число a = "))
+b = int(input("Введите число b = "))
+c = int(input("Введите число с = "))
+if ((a+b)==c):
+     print ("YES")
+else:
+     print ("NO")
+if ((-b/a)==c):
+     print ("YES")
+else:
+     print ("NO")
+     
