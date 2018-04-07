45bdc8b0e744a65c5908eff7247cf4f2c85c47cb
Арина Мельникова <31884600+melnikova12@users.noreply.github.com>
2018-04-07 12:37:27 +0300

    Update ex1.py

--- TEST2/ex1.py
+++ TEST2/ex1.py
@@ -12,10 +12,14 @@ with open (fname, encoding = 'utf-8') as f:
     f = f.read()
     
 #поиск символов
-search = re.findall(r'<se>.*</se>?', f)
+i = 0 
+search = re.findall(r'<se>.*</se>', f)
+for t in search:
+    i += 1
+    
 
 #запись в файл
 #файл для записи -  text.txt
 with open ('text.txt', 'w', encoding = 'utf-8') as f:
-    f = f.write(str(search))                   
+    f = f.write(str(i))                   
                   
