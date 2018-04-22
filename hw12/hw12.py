ded4db50a877c99872ed6fd1654de87497836c28
Арина Мельникова <31884600+melnikova12@users.noreply.github.com>
2018-04-22 11:24:20 +0300

    upload code

---
+++ hw12/hw12.py
@@ -0,0 +1,28 @@
+#вариант 1 
+import os
+import re
+
+print ('Данная программа показывает сколько найдено папок, содержащих цифры в названии')
+
+def find_folders():
+    i = 0
+    path = '../Documents'
+
+    #на всякий случай проверяю, существует ли указанный путь 
+    if os.path.exists(path):
+        print ('Все верно,данный путь существует.')
+    else:
+        print('Что-то пошло не так :(')
+    path = os.listdir()
+    for folder in path:
+        #if os.path.exists(folder):
+        if os.path.isdir(folder):
+            if re.findall('[0-9]', folder):
+                i += 1
+              #  continue
+       # if os.path.isdir(folder):
+       #     if re.findall('[0-9]', folder):
+       #         i = i+1
+    print ('Папок, в названии которых содержатся цифры: ', i )
+
+find_folders()
