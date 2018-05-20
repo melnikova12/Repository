718fb8b02601f22fca6ba20ba04faaa1a28e1518
Арина Мельникова <31884600+melnikova12@users.noreply.github.com>
2018-05-20 22:06:03 +0300

    changes

--- hw13/hw13.py
+++ hw13/hw13.py
@@ -15,5 +15,5 @@ for root, dirs, files in os.walk(start_path):
         max_d = max(deep)
         sp = re.findall( r'\\', start_path)
         len_sp = len(sp)
-        needed_depth = max_d - len_sp + 1
-print('Максимальная глубина папки в этом дереве: ',needed_depth)
+        final_depth = max_d - len_sp + 1
+print('Максимальная глубина папки в этом дереве: ',final_depth)
